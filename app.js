fb136eb05f69f2cf8a19bbdf3be60bce41e263da
yamacho <yamacho@users.noreply.github.com>
2020-03-09 18:35:08 +0900

    photos の Router オブジェクトを実装

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var photosRouter = require('./routes/photos');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/photos', photosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/photos.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.send('Some photos');
+});
+
+module.exports = router;
